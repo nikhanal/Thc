0e58a2e85d6d1257dc41031b6815e10965e593b7
Nishan Khanal <nishankhanal@Spiny-Babler.local>
2023-06-14 22:02:16 +0545

    Removed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import News from "../components/News";
 import Facts from "../components/Fact";
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
       <Head>
         <title>Thapathali Campus</title>
       </Head>
-      <News />
       <Facts />
     </>
   );
